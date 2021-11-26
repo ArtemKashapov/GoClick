b557f3e61f2512755f2d955be5c23e600adf2fef
Artem <ar.kashapov@gmail.com>
2021-11-26 23:34:51 +0400

    separate

--- main.go
+++ main.go
@@ -5,24 +5,18 @@ import (
 	"net/http"
 )
 
-// Создается функция-обработчик "home", которая записывает байтовый слайс, содержащий
-// текст "Hello World!" как тело ответа.
-func home(w http.ResponseWriter, r *http.Request) {
+func index_page(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello World!"))
 }
 
-func main() {
-	// Используется функция http.NewServeMux() для инициализации нового рутера, затем
-	// функцию "home" регистрируется как обработчик для URL-шаблона "/".
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
+func handleRequest() {
+	http.HandleFunc("/", index_page)
 
-	// Используется функция http.ListenAndServe() для запуска нового веб-сервера.
-	// Мы передаем два параметра: TCP-адрес сети для прослушивания (в данном случае это "localhost:4000")
-	// и созданный рутер. Если вызов http.ListenAndServe() возвращает ошибку
-	// мы используем функцию log.Fatal() для логирования ошибок. Обратите внимание
-	// что любая ошибка, возвращаемая от http.ListenAndServe(), всегда non-nil.
 	log.Println("Запуск веб-сервера на http://127.0.0.1:8000")
-	err := http.ListenAndServe(":8000", mux)
+	err := http.ListenAndServe(":8000", nil)
 	log.Fatal(err)
+}
+
+func main() {
+	handleRequest()
 }
