eeb238f1aa7b9c71d11cffa88a6190b4d614b426
Artem <ar.kashapov@gmail.com>
2021-11-26 23:38:09 +0400

    deleted comments

--- main.go
+++ main.go
@@ -6,8 +6,6 @@ import (
 	"net/http"
 )
 
-// Создается функция-обработчик "home", которая записывает байтовый слайс, содержащий
-// текст "Hello World!" как тело ответа.
 func home(w http.ResponseWriter, r *http.Request) {
 	tmpl, _ := template.ParseFiles("templates/index.html")
 	tmpl.Execute(w, nil)
@@ -15,7 +13,6 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 func handleRequest() {
 	http.HandleFunc("/", home)
-
 	log.Println("Запуск веб-сервера на http://127.0.0.1:8000")
 	err := http.ListenAndServe(":8000", nil)
 	log.Fatal(err)
