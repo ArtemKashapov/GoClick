d1af6260c1c7e999be3169173d0d3cc6cedf326f
Anton <voevodin_anton_2000@mail.ru>
2021-11-28 00:17:53 +0400

    add database

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	_ "github.com/lib/pq"
+
+	"database/sql"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+)
+
+const (
+	DB_USER     = "postgres"
+	DB_PASSWORD = "admin"
+	DB_NAME     = "postgres"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -19,5 +29,23 @@ func handleRequest() {
 }
 
 func main() {
+	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable",
+		DB_USER, DB_PASSWORD, DB_NAME)
+	db, err := sql.Open("postgres", dbinfo)
+	checkErr(err)
+	defer db.Close()
+
+	var lastInsertId int
+	err = db.QueryRow("INSERT INTO counter_info(counter) VALUES($1) returning id;", "1").Scan(&lastInsertId)
+	checkErr(err)
+	fmt.Println("last inserted id =", lastInsertId)
+
 	handleRequest()
+}
+
+func checkErr(err error) {
+
+	if err != nil {
+		panic(err)
+	}
 }
