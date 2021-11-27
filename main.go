bc34858292e04b78c7f60957d5ebefa184493982
Artem <ar.kashapov@gmail.com>
2021-11-27 21:13:44 +0400

    error handler1

--- main.go
+++ main.go
@@ -7,14 +7,17 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-	tmpl, _ := template.ParseFiles("templates/index.html")
+	tmpl, err := template.ParseFiles("templates/index.html")
 	tmpl.Execute(w, "Hello World!")
-	// log.Fatal(err)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func handleRequest() {
 	http.HandleFunc("/", home)
-	log.Println("Запуск веб-сервера на http://127.0.0.1:8000")
+	log.Println("Запуск сервера на http://127.0.0.1:8000")
 	err := http.ListenAndServe(":8000", nil)
 	log.Fatal(err)
 }
